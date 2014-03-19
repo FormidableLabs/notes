54e08c3f53c61c19f802d23b4b3eac233eaafd03
Ryan Roemer <ryan.roemer@formidablelabs.com>
2014-03-19 16:20:43 -0700

    Hook up jshint

--- full/amd/Gruntfile.js
+++ full/amd/Gruntfile.js
@@ -2,5 +2,29 @@
  * Gruntfile
  */
 module.exports = function (grunt) {
-  console.log("GRUNT");
+
+  // Helpers
+  /**
+   * Strip comments from JSON file (naive) and return JS object.
+   */
+  var _readJsonCfg = function (name) {
+    if (!grunt.file.exists(name)) { return "{}"; }
+    return JSON.parse(grunt.file.read(name).replace(/\/\/.*\n/g, ""));
+  };
+
+  // Configuration.
+  grunt.initConfig({
+    jshint: {
+      client: {
+        options: _readJsonCfg(".jshint.json"),
+        files: {
+          src: []
+        }
+      }
+    }
+  });
+
+  // Load vendor tasks.
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+
 };
