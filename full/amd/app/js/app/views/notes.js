6d9da44c686343129dfeed21e28483a6fd29ca02
Ryan Roemer <ryan.roemer@formidablelabs.com>
2014-03-30 06:47:47 -0700

    More unwrapping

--- full/amd/app/js/app/views/notes.js
+++ full/amd/app/js/app/views/notes.js
@@ -28,12 +28,8 @@ define([
     el: "#notes",
 
     events: {
-      "click    #note-create": function () {
-        this.createNote();
-      },
-      "keypress #note-new-input": function (ev) {
-        this.enterNote(ev);
-      }
+      "click    #note-create":    "createNote",
+      "keypress #note-new-input": "enterNote"
     },
 
     initialize: function () {
@@ -69,7 +65,7 @@ define([
       return this;
     },
 
-    // Add single child note view to front of notes list.
+    // Add single child note view to end of notes list.
     addNote: function (model) {
       var view = new NotesItemView({
         model: model
