1f04957cb52a971842415341fc7efb2ab7444564
Ryan Roemer <ryan.roemer@formidablelabs.com>
2014-03-30 07:11:57 -0700

    Start porting over note view tests

--- full/amd/test/jasmine/js/spec/views/note-view.spec.js
+++ full/amd/test/jasmine/js/spec/views/note-view.spec.js
@@ -1,7 +1,49 @@
-define(["jquery", "app/views/note"], function ($, NoteView) {
+define([
+  "jquery",
+  "app/views/note",
+  "app/models/note"
+], function ($, NoteView, NoteModel) {
+
   describe("app/views/note", function () {
-    console.log("TODO HERE FIXTURES", $("#fixtures").length);
+    beforeEach(function () {
+      // Create test fixture.
+      this.$fixture = $("<div id='note-view-fixture'></div>");
+
+      // Empty out and rebind the fixture for each run.
+      this.$fixture.empty().appendTo($("#fixtures"));
+
+      // New default model and view for each test.
+      //
+      // Creation actually calls `render()`, so in tests we have an
+      // *already rendered* view.
+      this.view = new NoteView({
+        el: this.$fixture,
+        model: new NoteModel()
+      });
+    });
+
+    afterEach(function () {
+      // Destroying the model also destroys the view.
+      this.view.model.destroy();
+
+      // Remove all sub-fixtures after test suite finishes.
+      $("#fixtures").empty();
+    });
+
+    it("can render an empty note", function () {
+      var $title = $("#pane-title"),
+        $text = $("#pane-text");
+
+      // Default to empty title in `h2` tag.
+      expect($title.text()).toBe("");
+      expect($title.prop("tagName")).toMatch(/h2/i);
+
+      // Have simple default message.
+      expect($text.text()).toBe("Edit your note!");
+      expect($text.html()).toBe("<p><em>Edit your note!</em></p>");
+    });
   });
+
 });
 
 /* Backbone Testing References
