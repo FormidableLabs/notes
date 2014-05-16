98622ea8137cc20d962cec12399b36340c043918
Mel Quark <mel.quark@gmail.com>
2014-05-16 09:33:24 -0700

    rest api to sqlite3 is working

--- full/amd/server/index.js
+++ full/amd/server/index.js
@@ -1,8 +1,6 @@
 var express = require("express"),
-  _ = require("lodash"),
   app = express(),
-  json = require('express-json'),
-  form = require('express-form2'),
+  bodyParser = require("body-parser"),
   sql = require("sqlite3"),
   db = new sql.Database("./server/notes.sqlite", sql.OPEN_READWRITE, function (error) {
     serverSetup();
@@ -10,34 +8,42 @@ var express = require("express"),
 
 function serverSetup () {
   app.use("/", express.static("app"));
+  app.use(bodyParser());
 
   app.get("/tasks", function (req, res) {
     db.prepare("select * from notes")
-      .all(
-        function (err, rows) {
-          res.json(rows);
+      .all(function (err, rows) {
+        res.json(rows);
       });
   });
 
   // TODO: sanitize input ???
   app.post("/tasks", function (req, res) {
-    console.log(req.body);
-    db.run("insert into notes (title, text) values(?,'')", title);
-    db.prepare("select * from notes order by id desc limit 1")
-      .all(
-        function (err, rows) {
-          res.json(rows);
+    var title = req.body.title || "",
+      text = req.body.text || "";
+    db.run("insert into notes (title, text) values(?,?)", title, text)
+      .prepare("select * from notes order by id desc limit 1")
+      .get(function (err, row) {
+        res.json(row);
       });
   });
 
+  // TODO: sanitize input ???
   app.put("/tasks/:id", function (req, res) {
-    var title = req.query.title,
-      text = req.query.text;
+    var title = req.body.title,
+      text = req.body.text,
+      id = req.params.id;
+    db.run("update notes set title=?, text=? where id=?", title, text, id)
+      .prepare("select * from notes where id=?", id)
+      .get(function (err, row) {
+        res.json(row);
+      });
   });
 
   app.delete("/tasks/:id", function (req, res, id) {
-    db.run("delete from notes where id=1");
+    db.run("delete from notes where id=?", req.params.id);
+    // TODO: what should the response be ???
   });
 
-  app.listen(3333);
+  app.listen(3000);
 }
