07f73bf6dd8ed3ac00365b9429d4c8295ee5ab9b
Mel Quark <mel.quark@gmail.com>
2014-05-16 08:47:00 -0700

    safety

--- full/amd/server/index.js
+++ full/amd/server/index.js
@@ -1,5 +1,8 @@
 var express = require("express"),
+  _ = require("lodash"),
   app = express(),
+  json = require('express-json'),
+  form = require('express-form2'),
   sql = require("sqlite3"),
   db = new sql.Database("./server/notes.sqlite", sql.OPEN_READWRITE, function (error) {
     serverSetup();
@@ -16,13 +19,24 @@ function serverSetup () {
       });
   });
 
-  app.post("/task", function (req, res) {
+  // TODO: sanitize input ???
+  app.post("/tasks", function (req, res) {
+    console.log(req.body);
+    db.run("insert into notes (title, text) values(?,'')", title);
+    db.prepare("select * from notes order by id desc limit 1")
+      .all(
+        function (err, rows) {
+          res.json(rows);
+      });
   });
 
-  app.put("/task/:id", function (req, res) {
+  app.put("/tasks/:id", function (req, res) {
+    var title = req.query.title,
+      text = req.query.text;
   });
 
-  app.delete("/task/:id", function (req, res) {
+  app.delete("/tasks/:id", function (req, res, id) {
+    db.run("delete from notes where id=1");
   });
 
   app.listen(3333);
