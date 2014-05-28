c589648d72ff3dc6add9ad12f953e2106b70d8f4
Ryan Roemer <ryan.roemer@formidablelabs.com>
2014-05-28 08:42:56 +0000

    Remove all test files to start

--- full/browserify/Gruntfile.js
+++ full/browserify/Gruntfile.js
@@ -12,52 +12,6 @@ module.exports = function (grunt) {
     return JSON.parse(grunt.file.read(name).replace(/\/\/.*\n/g, ""));
   };
 
-  // Declarations:
-  var KARMA_JASMINE_OPTIONS = {
-    runnerPort: 9999,
-    reporters: ["spec"],
-    frameworks: ["jasmine", "TODO_BROWSERIFY"],
-    files: [
-      // Test libraries.
-      "app/js/vendor/sinon.js", // TODO[BROWSERIFY]
-
-      // Adapters, config and test wrapper.
-      "app/js/config.js",
-      "test/jasmine/js/main-karma.js",
-
-      // Includes.
-      { pattern: "app/js/**/*.js",                included: false },
-      { pattern: "app/js/**/*.hbs",               included: false },
-      { pattern: "test/jasmine/js/spec/**/*.js",  included: false }
-    ]
-  };
-  var KARMA_MOCHA_OPTIONS = {
-    runnerPort: 9999,
-    reporters: ["spec"],
-    frameworks: ["mocha", "TODO_BROWSERIFY"],
-    files: [
-      // Test libraries.
-      "app/js/vendor/sinon.js", // TODO[BROWSERIFY]
-      // Chai / Sinon-Chai need async load. // TODO[BROWSERIFY]
-      { pattern: "app/js/vendor/chai.js",         included: false },
-      { pattern: "app/js/vendor/sinon-chai.js",   included: false },
-
-      // Adapters, config and test wrapper.
-      "app/js/config.js",
-      "test/mocha/js/main-karma.js",
-
-      // Includes.
-      { pattern: "app/js/**/*.js",                included: false },
-      { pattern: "app/js/**/*.hbs",               included: false },
-      { pattern: "test/mocha/js/spec/**/*.js",    included: false }
-    ],
-    client: {
-      mocha: {
-        ui: "bdd"
-      }
-    }
-  };
-
   // Configuration.
   grunt.initConfig({
 
@@ -78,7 +32,7 @@ module.exports = function (grunt) {
     // JsHint style checks.
     // ------------------------------------------------------------------------
     jshint: {
-      options: _readJsonCfg(".jshint.json"),
+      options: _readJsonCfg(".jshint-frontend.json"),
       client: {
         files: {
           src: [
@@ -94,6 +48,15 @@ module.exports = function (grunt) {
         files: {
           src: [
             "test/*/js/**/*.js"
+          ]
+        }
+      },
+      server: {
+        options: _readJsonCfg(".jshint-backend.json"),
+        files: {
+          src: [
+            "*.js",
+            "server/**/*.js"
           ]
         }
       }
@@ -126,74 +89,8 @@ module.exports = function (grunt) {
     },
 
     // ------------------------------------------------------------------------
-    // Karma test driver.
-    // ------------------------------------------------------------------------
-    karma: {
-      "mocha-fast": {
-        options: KARMA_MOCHA_OPTIONS,
-        singleRun: true,
-        browsers: ["PhantomJS"]
-      },
-      "jasmine-fast": {
-        options: KARMA_JASMINE_OPTIONS,
-        singleRun: true,
-        browsers: ["PhantomJS"]
-      },
-      "mocha-windows": {
-        options: KARMA_MOCHA_OPTIONS,
-        singleRun: true,
-        browsers: ["PhantomJS", "IE", "Chrome"]
-      },
-      "jasmine-windows": {
-        options: KARMA_JASMINE_OPTIONS,
-        singleRun: true,
-        browsers: ["PhantomJS", "IE", "Chrome"]
-      },
-      "mocha-ci": {
-        options: KARMA_MOCHA_OPTIONS,
-        singleRun: true,
-        browsers: ["PhantomJS", "Firefox"]
-      },
-      "jasmine-ci": {
-        options: KARMA_JASMINE_OPTIONS,
-        singleRun: true,
-        browsers: ["PhantomJS", "Firefox"]
-      },
-      "mocha-all": {
-        options: KARMA_MOCHA_OPTIONS,
-        singleRun: true,
-        browsers: ["PhantomJS", "Chrome", "Firefox", "Safari"]
-      },
-      "jasmine-all": {
-        options: KARMA_JASMINE_OPTIONS,
-        singleRun: true,
-        browsers: ["PhantomJS", "Chrome", "Firefox", "Safari"]
-      },
-      "jasmine-dev": {
-        // Runs tests automatically on changes in ongoing terminal.
-        options: KARMA_JASMINE_OPTIONS,
-        browsers: ["PhantomJS", "Chrome", "Firefox", "Safari"]
-      },
-      "mocha-dev": {
-        // Runs tests automatically on changes in ongoing terminal.
-        options: KARMA_MOCHA_OPTIONS,
-        browsers: ["PhantomJS", "Chrome", "Firefox", "Safari"]
-      }
-    },
-
-    // ------------------------------------------------------------------------
     // Development servers.
     // ------------------------------------------------------------------------
-    // Full REST backend with Express.
-    nodemon: {
-      dev: {
-        script: "server/index.js"
-      },
-      options: {
-        watch: ["server"]
-      }
-    },
-
     // Pure static (localStorage) server.
     connect: {
       // Run examples server at: http://127.0.0.1:9874
@@ -211,32 +108,33 @@ module.exports = function (grunt) {
   // Load dependencies.
   grunt.loadNpmTasks("grunt-contrib-clean");
   grunt.loadNpmTasks("grunt-browserify");
-  grunt.loadNpmTasks("grunt-nodemon");
   grunt.loadNpmTasks("grunt-contrib-connect");
   grunt.loadNpmTasks("grunt-contrib-jshint");
-  grunt.loadNpmTasks("grunt-karma");
 
   // --------------------------------------------------------------------------
   // Tasks: Build
   // --------------------------------------------------------------------------
   grunt.registerTask("build", [
     "clean:dist",
-    "copy:dist"
-    // TODO[BROWSERIFY]
+    "browserify:dist"
   ]);
 
   // --------------------------------------------------------------------------
   // Tasks: QA
   // --------------------------------------------------------------------------
+  /* TODO
   grunt.registerTask("karma:fast",  ["karma:mocha-fast", "karma:jasmine-fast"]);
   grunt.registerTask("karma:ci",    ["karma:mocha-ci", "karma:jasmine-ci"]);
   grunt.registerTask("karma:all",   ["karma:mocha-all", "karma:jasmine-all"]);
+  */
 
-  grunt.registerTask("test",        ["karma:fast"]);
+  grunt.registerTask("test",        []); // TODO["karma:fast"]);
 
   grunt.registerTask("check",       ["jshint", "test"]);
+  /* TODO
   grunt.registerTask("check:ci",    ["jshint", "karma:ci"]);
   grunt.registerTask("check:all",   ["jshint", "karma:all"]);
+  */
 
   // --------------------------------------------------------------------------
   // Tasks: Default
--- full/browserify/server/init-db.js
+++
@@ -1,7 +0,0 @@
-var sql = require("sqlite3"),
-  db = new sql.Database(__dirname + "/notes.sqlite", sql.OPEN_READWRITE | sql.OPEN_CREATE, function (error) {
-    db.run("drop table if exists notes", function () {
-      db.run("create table notes (id integer primary key autoincrement, title text, text text)");
-    });
-    db.close();
-  });
