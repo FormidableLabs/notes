ffe1d7f528a743cf2e879b681d8277c54042400f
Ryan Roemer <ryan.roemer@formidablelabs.com>
2014-06-12 21:26:01 -0700

    Move app-specific config to deps.js and get skeleton working.

---
+++ skeleton/browserify/test/mocha/js/spec/hello.spec.js
@@ -0,0 +1,12 @@
+/**
+ * A simple "hello world" test file (with no app dependencies yet).
+ */
+describe("My Test Suite", function () {
+  it("should be true", function () {
+    expect(true).to.be.true;
+  });
+
+  it("should be false", function () {
+    expect(false).to.be.false;
+  });
+});
