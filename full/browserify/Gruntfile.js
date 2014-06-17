8130e6747ffd869cd63675c8bc14931a45180214
Ryan Roemer <ryan.roemer@formidablelabs.com>
2014-06-17 08:50:46 -0700

    jshint fix

--- full/browserify/Gruntfile.js
+++ full/browserify/Gruntfile.js
@@ -154,7 +154,7 @@ module.exports = function (grunt) {
     // ------------------------------------------------------------------------
     browserify: _.merge({
       options: {
-        transform: ["hbsfy"],
+        transform: ["hbsfy"]
       }
     }, BUNDLES, BUNDLES_WATCH),
 
