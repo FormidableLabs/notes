0743949ad737ae406fe594bc6800ff207eafacdf
Ryan Roemer <ryan@loose-bits.com>
2014-08-21 10:05:47 -0700

    Fix jshint task

--- full/webpack-cjs/gulpfile.js
+++ full/webpack-cjs/gulpfile.js
@@ -2,6 +2,7 @@
  * Gulpfile
  */
 var fs = require("fs");
+var _ = require("lodash");
 var gulp = require("gulp");
 var jshint = require("gulp-jshint");
 var webpack = require("gulp-webpack");
@@ -18,10 +19,25 @@ var _jsonCfg = function (name) {
 // ----------------------------------------------------------------------------
 // JsHint
 // ----------------------------------------------------------------------------
-gulp.task("jshint:frontend", function () {
+gulp.task("jshint:client", function () {
   gulp
-    .src("app/js/app/**/*.js")
+    .src([
+      "app/js/*.js",
+      "app/js/app/**/*.js"
+    ])
     .pipe(jshint(_jsonCfg(".jshint-frontend.json")))
+    .pipe(jshint.reporter("default"))
+    .pipe(jshint.reporter("fail"));
+});
+
+gulp.task("jshint:test", function () {
+  gulp
+    .src([
+      "test/*/js/**/*.js"
+    ])
+    .pipe(jshint(_.extend(_jsonCfg(".jshint-frontend.json"), {
+      "es3": false // Allow old-IE breaking variations for `to.be.true`.
+    })))
     .pipe(jshint.reporter("default"))
     .pipe(jshint.reporter("fail"));
 });
@@ -37,7 +53,7 @@ gulp.task("jshint:backend", function () {
     .pipe(jshint.reporter("fail"));
 });
 
-gulp.task("jshint", ["jshint:frontend", "jshint:backend"]);
+gulp.task("jshint", ["jshint:client", "jshint:test", "jshint:backend"]);
 
 // ----------------------------------------------------------------------------
 // Builders
@@ -57,4 +73,3 @@ gulp.task("server", function () {
 // ----------------------------------------------------------------------------
 gulp.task("check",    ["jshint"]);
 gulp.task("default",  ["check"]);
-
